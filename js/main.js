8b0f5ce6ce8e9f76032b32bd129fe9cf1e9bcef7
Jun Hashimoto <jun.hashimoto.0204@gmail.com>
2024-06-12 08:06:48 +0900

    remove ga send function

--- js/main.js
+++ js/main.js
@@ -243,7 +243,6 @@ var setTimeBuffer = function (time) {
 
 var navigate = function (url) {
     if(url!=""){
-        sendAnalytics();
         chrome.tabs.getSelected(null,function(tab){
             chrome.tabs.update(
                 tab.id,
@@ -251,15 +250,4 @@ var navigate = function (url) {
             );
         });
     }
-};
-
-var sendAnalytics = function() {
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-38969973-1']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
 };
